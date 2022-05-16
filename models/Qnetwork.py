9eb7489277b614881b0fbd24fdf56f1372ba714f
SANGMIN LEE <92682815+san9min@users.noreply.github.com>
2022-05-16 21:34:14 +0900

    Init Structure

---
+++ models/Qnetwork.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class Qnetwork(nn.Module):
+    def __init__(self):
+        super(Qnetwork, self).__init__()
+
+        #TODO
+
+    def forward(self,x):
+
+        return 
