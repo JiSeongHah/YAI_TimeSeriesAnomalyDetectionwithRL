34e0798b8d7be8201aa2e3dbe7af4e2619d34e93
SANGMIN LEE <92682815+san9min@users.noreply.github.com>
2022-05-26 20:13:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def main(args):
     elif args.dataset == 'Numenta':
         pass
     
+    # 데이터 어떻게 찍히는지 보고 감잡기
     for time_stamp, value, label in (train_loader):
         print(f'time_stamp : {time_stamp}')
-        print(f'value : {value},{value.size}')
+        print(f'value : {value},{value.size()}')
         print(f'label : {label},{label.size()}')
-        print('-'*50)
+        print('-'*80)
 
 
 
