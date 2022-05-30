9d8d4c1d883a2b774d9b37c2a4511e8df0f654e1
chanmi2 <lcm0419@yonsei.ac.kr>
2022-05-31 00:57:28 +0900

    220530

---
+++ config.py
@@ -0,0 +1,24 @@
+import argparse
+from platform import java_ver
+import torch
+
+# Params
+def get_parse():
+    args = argparse.Namespace()
+
+    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    #data
+    args.datasets = 'Yahoo'  # Yahoo, SWaT, Numenta, KPI
+    
+    args.data_path = 'G:\내 드라이브\YAI\AD_for_TS_w_RL\data\Yahoo\ydata-labeled-time-series-anomalies-v1_0'
+
+    args.shuffle = False
+    args.window_size = 50   
+    args.split_ratio = 0.8
+
+
+    #parameters
+    args.batch_size = 150
+
+    return args    
---
+++ datasets/Yahoo.py
@@ -0,0 +1,100 @@
+import torch
+import pandas as pd 
+import numpy as np
+
+from torch.utils.data import Dataset, DataLoader
+
+import os
+import glob
+
+from util.sliding_window import sliding_window 
+
+
+def build_yahoo(args): 
+    train, test = Yahoo_Dataprocessing(args) # divide data into train, test : list of sets
+
+    train = sliding_window(train, args)  # sliding window
+    test = sliding_window(test, args)
+
+    train = YahooDataset(train)
+    test = YahooDataset(test)
+
+    train_loader = DataLoader(train, batch_size = args.batch_size, shuffle = args.shuffle, num_workers = 2)
+    test_loader = DataLoader(test, batch_size = args.batch_size, shuffle = args.shuffle, num_workers = 2) 
+
+    return train_loader, test_loader
+
+class YahooDataset(Dataset):
+
+    def __init__(self, dataset):
+        super(YahooDataset,self).__init__()
+
+        self.timestamp = dataset['timestamp']
+        self.value = dataset['value']
+        self.label = dataset['label']
+
+    def __len__(self):
+        return len(self.timestamp)
+
+    def __getitem__(self,idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        timestamp = self.timestamp[idx]
+        value = self.value[idx]
+        label = self.label[idx]
+
+        time_stamp = np.array(timestamp)
+        value = np.array(value) 
+        label = np.array(label)
+
+        # normalize
+        value = (value - value.mean()) / value.std()
+
+        return time_stamp, value, label
+
+
+def Yahoo_Dataprocessing(args):
+    
+    files_a1 = glob.glob(os.path.join(args.data_path,"A1Benchmark/real_*.csv"))  #list
+    files_a1.sort()
+    files_a2 = glob.glob(os.path.join(args.data_path,"A2Benchmark/synthetic_*.csv"))
+    files_a2.sort()
+
+    split_bar = np.array([len(files_a1),len(files_a2)])
+    split_bar = args.split_ratio * split_bar
+    split_bar = np.asarray(split_bar,dtype = int)
+    
+    train = []    
+    test = []
+    for i, fn  in enumerate(files_a1):
+        df = pd.read_csv(fn)
+        if i < split_bar[0]: # train
+            train.append({
+            'timestamp': (1483225200 + 3600 * df['timestamp']).tolist(),
+            'value': df['value'].tolist(),
+            'label': df['is_anomaly'].tolist()
+            })
+        else: #test
+            test.append({
+            'timestamp': (1483225200 + 3600 * df['timestamp']).tolist(),
+            'value': df['value'].tolist(),
+            'label': df['is_anomaly'].tolist()
+            })
+    for i, fn in enumerate(files_a2):
+        df = pd.read_csv(fn)
+        if i < split_bar[1]: # train
+            train.append({
+                'timestamp': df['timestamp'].tolist(),
+                'value': df['value'].tolist(),
+                'label': df['is_anomaly'].tolist()
+                })
+        else: #tesst
+            test.append({
+            'timestamp': (1483225200 + 3600 * df['timestamp']).tolist(),
+            'value': df['value'].tolist(),
+            'label': df['is_anomaly'].tolist()
+            })
+    assert len(train) + len(test)  == 167, 'Error'
+
+    return train, test
